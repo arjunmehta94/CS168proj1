01e91751e749c882a9c645d06bc58deb93312a05
Arjun Mehta <arjunmehta94@berkeley.edu>
2015-09-30 01:01:00 -0700

    added miscellaneous checks: INFINITY in handle rx and < 0 in handle link up

--- proj_routing/simulator/dv_router.py
+++ proj_routing/simulator/dv_router.py
@@ -13,7 +13,7 @@ INFINITY = 16
 class DVRouter (basics.DVRouterBase):
   #NO_LOG = True # Set to True on an instance to disable its logging
   #POISON_MODE = True # Can override POISON_MODE here
-  DEFAULT_TIMER_INTERVAL = 20 # Can override this yourself for testing
+  #DEFAULT_TIMER_INTERVAL = 20 # Can override this yourself for testing
 
   def __init__ (self):
     """
@@ -34,7 +34,7 @@ class DVRouter (basics.DVRouterBase):
     The port attached to the link and the link latency are passed in.
     """
     if port not in self.port_table:
-      if not latency >= INFINITY:
+      if not latency >= INFINITY and not latency <= 0:
         self.port_table[port] = latency
 
   def handle_link_down (self, port):
@@ -73,8 +73,8 @@ class DVRouter (basics.DVRouterBase):
         # print self.port_table[port]
         # print packet.latency
         distance = packet.latency + self.port_table[port]
-        if packet.destination not in self.distance_vectors:
-          if not distance >= INFINITY:
+        if not distance >= INFINITY:
+          if packet.destination not in self.distance_vectors:
             self.distance_vectors[packet.destination] = []
             self.distance_vectors[packet.destination].append(distance)
             self.distance_vectors[packet.destination].append(port)
@@ -82,26 +82,30 @@ class DVRouter (basics.DVRouterBase):
             self.distance_vectors[packet.destination].append(False)
             route_packet = basics.RoutePacket(packet.destination, distance)
             self.send(route_packet, port, True)
-        else:
+          else:
 
-          # if current_time - self.distance_vectors[packet.destination][2] > 15 and not self.distance_vectors[packet.destination][3]:
-          #   #print str(self) + str(destination)
-          #   del self.distance_vectors[packet.destination]
-          #else:
-          curr_distance = self.distance_vectors[packet.destination][0]
-          if curr_distance > distance:
-            curr_distance = distance
-            self.distance_vectors[packet.destination][0] = curr_distance
-            self.distance_vectors[packet.destination][1] = port
-            self.distance_vectors[packet.destination][3] = False
-            route_packet = basics.RoutePacket(packet.destination, curr_distance)
-            self.send(route_packet, port, True)
-          self.distance_vectors[packet.destination][2] = current_time
-          # stored distance should never be infinity
+            # if current_time - self.distance_vectors[packet.destination][2] > 15 and not self.distance_vectors[packet.destination][3]:
+            #   #print str(self) + str(destination)
+            #   del self.distance_vectors[packet.destination]
+            #else:
+            curr_distance = self.distance_vectors[packet.destination][0]
+            if curr_distance > distance:
+              curr_distance = distance
+              self.distance_vectors[packet.destination][0] = curr_distance
+              self.distance_vectors[packet.destination][1] = port
+              self.distance_vectors[packet.destination][2] = current_time
+              self.distance_vectors[packet.destination][3] = False
+              route_packet = basics.RoutePacket(packet.destination, curr_distance)
+              self.send(route_packet, port, True)
+            elif curr_distance == distance:
+              if port is not self.distance_vectors[packet.destination][1]:
+                self.distance_vectors[packet.destination][1] = port
+              self.distance_vectors[packet.destination][2] = current_time
+            # stored distance should never be infinity
 
-          # POISON MODE IMPLEMENTATION
-          # check if distance is actually infinite and delete that from your table
-          
+            # POISON MODE IMPLEMENTATION
+            # check if distance is actually infinite and delete that from your table
+            
     elif isinstance(packet, basics.HostDiscoveryPacket):
       if port in self.port_table:
         self.distance_vectors[packet.src] = []
@@ -117,7 +121,8 @@ class DVRouter (basics.DVRouterBase):
       #self.send(packet, port=port)
       if packet.dst in self.distance_vectors:
         outport = self.distance_vectors[packet.dst][1]
-        self.send(packet, outport)
+        if outport != port:
+          self.send(packet, outport)
 
   def handle_timer (self):
     """
@@ -129,14 +134,14 @@ class DVRouter (basics.DVRouterBase):
     current_time = api.current_time()
     keys_to_delete = []
     for destination in self.distance_vectors:
-      if current_time - self.distance_vectors[destination][2] > 15 and not self.distance_vectors[destination][3]:
+      if (current_time - self.distance_vectors[destination][2] > 15 and not self.distance_vectors[destination][3]) or self.distance_vectors[destination][0] >= INFINITY:
         keys_to_delete.append(destination)
       else:
-        print destination
+        #print destination
         route_packet = basics.RoutePacket(destination, self.distance_vectors[destination][0])
         self.send(route_packet, None, True)
     for key in keys_to_delete:
-       print "deleting"
+       #print "deleting"
        del self.distance_vectors[key]
         ## IMPLEMENT POISON REVERSE, BROAD
       
